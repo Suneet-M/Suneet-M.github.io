4667fe3fa6358be540f9550b7b40915b5ba3499c
Suneet-M <43340251+Suneet-M@users.noreply.github.com>
2018-12-07 00:43:38 +0530

    fix: Sticky header was activating before or after intended in some devices
    - Earlier the pageYOffset was being compared to a pixel height which was about the same for all devices.
    - But some non 16:9 resolution devices would have different pixel position for the title and thus were displaying the sticky bar before or after intended.
    - Now the pageYOffset is directly compared to titleOffset.
    - Title offset is taken in a variable as soon as page loads because after activating sticky header the offset would always be 0 and sticky header would never be deactivated.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const browser = navigator.userAgent;
 if (browser.includes('Android') || browser.includes('iPhone') && browser.includes('Mobile')) {
+		const titleOffset = $('.title').offset().top;
 		$(window).scroll(function() {
-			if (window.pageYOffset >= 275) {
+			if (window.pageYOffset >= titleOffset) {
 				$('header').addClass('sticky');
 				$('.logo').hide();
 			}
